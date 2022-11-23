aa556a2d6d111acb75bad60e0ff81acbcf128556
HYEOK9 <leejaehyuck9@gmail.com>
2022-11-23 18:41:24 +0900

    feat: add eslint & prettier config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Button from "@mui/material/Button";
+import Button from '@mui/material/Button';
 const Home = () => {
     return (
         <div>
             <Button variant="text">Text</Button>
             <Button variant="contained">Contained</Button>
-            <Button variant="outlined">Outlined</Button>{" "}
+            <Button variant="outlined">Outlined</Button>
         </div>
     );
 };
